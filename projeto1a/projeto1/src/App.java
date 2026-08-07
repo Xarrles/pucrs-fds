90928b4412bf3101a3aaf5a1a2da4d24c90af24e
Igor Xarless <95253660+Xarrles@users.noreply.github.com>
2026-08-07 01:22:05 +0000

    fix: move app para dentro do src

--- projeto1a/projeto1/src/App.java
+++ projeto1a/projeto1/src/App.java
@@ -1,5 +1,10 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
+package projeto1a.projeto1.src;
+public class App { 
+public static void main(String[] args) { 
+System.out.println("\nHello world!"); 
+String version = System.getProperty("java.version"); 
+System.out.println("Running Java Version: "+version+"\n"); 
+String name = System.getProperty("user.name"); 
+System.out.println("User account name: "+name+"\n"); 
+} 
+} 
